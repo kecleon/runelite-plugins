561a0aceb7390c2e8a1ac71345f8f5e4fe698e31
kecleon <ultimatedood@gmail.com>
2024-07-03 06:13:00 -0700

    fix class name

--- src/test/java/com/partycustompings/PartyCustomPingsPluginTest.java
+++ src/test/java/com/partycustompings/PartyCustomPingsPluginTest.java
@@ -3,7 +3,7 @@ package com.partycustompings;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class PlayerOutlinePluginTest
+public class PartyCustomPingsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
